916525c1ab3b95c08281935f64cdf342a60625f1
joanmonzo <joan.monzo@orbelgrupo.com>
2026-05-12 16:44:21 +0200

    arreglo Entrada

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './WelcomeScreen.css'; // Asegúrate de actualizar también el CSS
+import './WelcomeScreen.css';
 
 export default function WelcomeScreen({ onEnter }) {
     // La ruta de la imagen en /public
@@ -7,33 +7,33 @@ export default function WelcomeScreen({ onEnter }) {
 
     return (
         <div className="welcome-overlay">
-            <div className="welcome-frame">
-                <p className="welcome-instruction">Pulsa sobre tu equipo para entrar</p>
 
-                <div className="image-selector-container">
-                    <img
-                        src={imageSelector}
-                        alt="Selección de Equipos"
-                        className="selector-image"
-                    />
+            <p className="welcome-instruction">Pulsa sobre tu equipo para entrar</p>
 
-                    {/* ZONAS INVISIBLES CLICKABLES (OVERLAYS) */}
+            <div className="image-selector-container">
+                <img
+                    src={imageSelector}
+                    alt="Selección de Equipos"
+                    className="selector-image"
+                />
 
-                    {/* Zona 1: Equipo Carabassa Slice (Izquierda) */}
-                    <div
-                        className="click-zone zone-left"
-                        onClick={onEnter}
-                        title="Entrar con Equipo Carabassa Slice Fockers"
-                    />
+                {/* ZONAS INVISIBLES CLICKABLES (OVERLAYS) */}
 
-                    {/* Zona 2: Equipo Carajillos (Derecha) */}
-                    <div
-                        className="click-zone zone-right"
-                        onClick={onEnter}
-                        title="Entrar con Equipo Carajillos Voladores"
-                    />
-                </div>
+                {/* Zona 1: Equipo Dorado (Izquierda) */}
+                <div
+                    className="click-zone zone-left"
+                    onClick={onEnter}
+                    title="Entrar con Equipo Dorado"
+                />
+
+                {/* Zona 2: Equipo Rojo (Derecha) */}
+                <div
+                    className="click-zone zone-right"
+                    onClick={onEnter}
+                    title="Entrar con Equipo Rojo"
+                />
             </div>
+
         </div>
     );
 }
